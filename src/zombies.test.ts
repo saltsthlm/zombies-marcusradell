c524f2aef8f9f57c5dc07ba4cf28efdca82907b2
Marcus Rådell <marcus@radell.net>
2024-09-09 13:27:10 +0200

    added test: empty room cannot fit any zombies

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -19,6 +19,8 @@ test("room is not full", () => {
 
 test.skip("empty room that fits one zombie is not full", () => {});
 
+test.skip("empty room cannot fit any zombies", () => {});
+
 test.skip("one-roomer becomes full when a zombie is added", () => {});
 
 test.skip("two-roomer is not full when a zombie is added", () => {});
