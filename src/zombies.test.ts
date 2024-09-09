7c2d4bf16ceccf1da8e9d4989131abf7e37a22e3
Marcus Rådell <marcus@radell.net>
2024-09-09 13:24:54 +0200

    add test case for one-roomer with zombie added

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -18,3 +18,5 @@ test("room is not full", () => {
 });
 
 test.skip("empty room that fits one zombie is not full", () => {});
+
+test.skip("one-roomer becomes full when a zombie is added", () => {});
